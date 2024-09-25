eebd502d731def90e0f5eb54f672cb70810333a5
Ruslan Dudchenko <55272228+dudchenko610@users.noreply.github.com>
2024-09-25 14:48:41 +0300

    added getScreenBounds method

--- Client/StripeIntegration.Components/wwwroot/js/common.js
+++ Client/StripeIntegration.Components/wwwroot/js/common.js
@@ -2,6 +2,16 @@ function isMobileDevice() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
+function getScreenBounds() {
+    const width = window.innerWidth ? document.documentElement.clientWidth : document.body.clientWidth;
+    const height = window.innerHeight ? document.documentElement.clientHeight : document.body.clientHeight;
+
+    return {
+        x: width,
+        y: height
+    }
+}
+
 function goBack() {
     window.history.back();
 }
